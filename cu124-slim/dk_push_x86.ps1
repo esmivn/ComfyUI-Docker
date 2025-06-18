f3d8ce66e89969fa9c4174ee754c5fb35437e639
esmivn <esmivn@gmail.com>
2025-06-17 23:14:59 -0400

    added dk_push_x86, added wan2.1 Q8 to download-models.txt

---
+++ cu124-slim/dk_push_x86.ps1
@@ -0,0 +1,13 @@
+# allow script execution:
+# Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+# 1) create and switch to a new builder instance
+docker buildx create --name winbuilder --use
+
+# 2) build for linux/amd64, tag it and push
+docker buildx build `
+  -f .\Dockerfile `
+  --platform linux/amd64 `
+  -t esmivn/comfyui-boot:cu124-slim-1 `
+  --push `
+  .
---
+++ cu124-slim/dk_push_x86.sh
@@ -0,0 +1,2 @@
+sudo docker buildx create --use
+sudo docker buildx build -f Dockerfile --platform linux/amd64 -t esmivn/comfyui-boot:cu124-slim-1 --push .
